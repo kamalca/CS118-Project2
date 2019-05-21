f585c7891746c8ebb373705d8b9fd37a5af67fa1
Kenna Wang <kenna.wang6@gmail.com>
2019-05-21 14:15:12 -0700

    Made basic client structure

--- clientsource.c
+++ clientsource.c
@@ -4,9 +4,22 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <netinet/in.h>
+#include "header.h"
 
-void transmit(){
+int transmit(int file){
+  //this function will be used to split the file into smaller sections and send to server
+  return -1;
+}
 
+int handshake(struct sockaddr* address){
+  //send syn
+  //wait for ack
+  return -1;
+}
+
+int iptoint(char* host){
+  return -1;
 }
 
 int stringtoint(char* string){
@@ -23,7 +36,7 @@ int stringtoint(char* string){
 int main(int argc, char* argv[]){
 
   //handling command line arguments
-  char* servername;
+  int servername;
   int port;
   char* filename;
 
@@ -31,28 +44,57 @@ int main(int argc, char* argv[]){
     fprintf(stderr, "Incorrect number of arguments.\nclient should be invoked with three arguments as\n./client <HOSTNAME-OR-IP> <PORT> <FILENAME\n");
     exit(1);
   }
-  servername = argv[1];
+
+  if (strcmp(argv[1], "localhost") == 0)
+    servername = 0x7f000001;
+  else{
+    servername = iptoint(argv[1]);
+    if (servername == -1){
+      fprintf(stderr, "Improper hostname\n");
+      exit(1);
+    }
+  }
+
   port = stringtoint(argv[2]);
   if (port < 0 || port > 65535){
     fprintf(stderr, "Improper port number\n");
     exit(1);
   }
+
   filename = argv[3];
+  int filefd = open(filename, O_RDONLY);
+  if (filefd < 0){
+    fprintf(stderr, "Couldn't open file %s\n", filename);
+    exit(1);
+  }
 
   //setting up socket
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  //socklen_t size = sizeof(struct sockaddr_in);
-  //struct sockaddr_in their_addr;
-
-  //opening target file
-  int filefd = open(filename, O_RDONLY);
-  if (filefd < 0)
-    exit(1);
+  socklen_t size = sizeof(struct sockaddr_in);
+  struct sockaddr_in their_addr;
+  their_addr.sin_family = AF_INET;
+  their_addr.sin_port = htons(port);
+  their_addr.sin_addr.s_addr = htonl(servername);
+  memset(&their_addr, 0, sizeof(their_addr));
+  struct sockaddr* addr = (struct sockaddr*) &their_addr;
 
   //do handshake
+  if (handshake(addr) == -1){
+    fprintf(stderr, "handshake failed\n");
+    close(sockfd);
+    exit(1);
+  }
+
   //split up file, send in packets
+  if (transmit(filefd) == -1){
+    fprintf(stderr, "transmission failed\n");
+    close(sockfd);
+    exit(1);
+  }
+
   //closing socket
   close(sockfd);
+  close(filefd);
 
   return 0;
 }
