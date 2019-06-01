dd8d6a7aec05c876edb648c7bec146e4d40d32cf
Kenna Wang <kenna.wang6@gmail.com>
2019-06-01 14:48:55 -0700

    Converted transmit function to alternate between send and receive while loops.

--- clientsource.c
+++ clientsource.c
@@ -67,51 +67,71 @@ int fin(int sockfd, struct sockaddr* address, unsigned short* seqNum, unsigned s
 
 int transmit(int file, int sockfd, struct sockaddr* address, unsigned short* seqNum, unsigned short* ackNum){
     //this function will be used to split the file into smaller sections and send to server
-    int numRead, offset = 0, cwnd = 512, ssthresh = 10240;
-    struct packet message, ack;
-    message.ack = 1;
-    message.syn = message.fin = 0;
-    memset(message.zeros, 0, sizeof(message.zeros));
+    int numRead, offset = 0, cwnd = 512, ssthresh = 5120, inprogress = 0, first = 1, sendbase = *seqNum, nextseq = *seqNum;
+    struct packet ack;
     fprintf(stderr, "transmit\n");
-    do{//use alarm() function with signal handler for SIGALRM, also hinted at using C++ std::chrono
-        numRead = pread(file, message.message, PAYLOAD, offset);
-
-        if (numRead == -1){
-            fprintf(stderr, "Error reading file, %s\n", strerror(errno));
-            return -1;
-        }
-
-        //write(1, message.message, numRead);//for debugging purposes
-        message.seqNum = *seqNum;
-        message.ackNum = *ackNum;
-
-        if(sendto(sockfd, (void*) &message, 12 + numRead, 0, address, sizeof(*address)) == -1){
-            fprintf(stderr, "Couldn't send packet to server, %s\n", strerror(errno));
-            return -1;
-        }
-        printsent(&message, cwnd, ssthresh);
-
-        //receive ack from server
-        if (recvfrom(sockfd, (void*) &ack, 12, 0, NULL, NULL) == -1){
-            fprintf(stderr, "Couldn't receive ack from server, %i\n", errno);
-            return -1;
-        }
-        printreceived(&ack, cwnd, ssthresh);
+    //initialize queue
+    
+    //main loop, alternates between sending and receiving loops
+    while(1){
         
-        //check server ack
-        (*seqNum) += numRead;
-        if (ack.ackNum != *seqNum){
-            fprintf(stderr, "Wrong ack ackNum, expected ackNum %i\n", *seqNum);
-            return -1;
-        } printf("seqNum = %i\n", *seqNum);
-        if (ack.seqNum != *ackNum){
-            fprintf(stderr, "Wrong ack seqNum, expected seqNum %i\n", *ackNum);
-            return -1;
+        //send loop
+        while(inprogress < cwnd){ //(queue.size < cwnd / 512)
+            //go through queue and check times, retransmit?
+            
+            //prepare message
+            struct packet* message = (packet*) malloc(sizeof(packet));
+            if (first)
+                message->ack = 1;
+            else
+                message->ack = 0;
+            message->syn = message->fin = 0;
+            memset(message->zeros, 0, sizeof(message->zeros));
+            message->seqNum = *seqNum;
+            message->ackNum = *ackNum;
+            
+            numRead = pread(file, message->message, PAYLOAD, offset);
+            if (numRead == -1){
+                fprintf(stderr, "Error reading file, %s\n", strerror(errno));
+                free(message);
+                //delete queue
+                return -1;
+            }
+            
+            if(sendto(sockfd, (void*) message, 12 + numRead, 0, address, sizeof(*address)) == -1){
+                fprintf(stderr, "Couldn't send packet to server, %s\n", strerror(errno));
+                free(message);
+                //delete queue
+                return -1;
+            }
+            //push message to queue
+            printsent(message, cwnd, ssthresh);
         }
-
+        
+        //receive loop
+        while(1){
+            /*receive ack from server
+             if (recvfrom(sockfd, (void*) &ack, 12, 0, NULL, NULL) == -1){
+             fprintf(stderr, "Couldn't receive ack from server, %i\n", errno);
+             return -1;
+             }
+             printreceived(&ack, cwnd, ssthresh);
+             
+             //check server ack
+             (*seqNum) += numRead;
+             if (ack.ackNum != *seqNum){
+             fprintf(stderr, "Wrong ack ackNum, expected ackNum %i\n", *seqNum);
+             return -1;
+             } printf("seqNum = %i\n", *seqNum);
+             if (ack.seqNum != *ackNum){
+             fprintf(stderr, "Wrong ack seqNum, expected seqNum %i\n", *ackNum);
+             return -1;
+             }*/
+        }
         offset += numRead;
         message.ack = 0;
-    }while (numRead == PAYLOAD);
+    }
+    
     fprintf(stderr, "transmission complete\n\n\n");
     return 0;
 }
@@ -120,12 +140,12 @@ int transmit(int file, int sockfd, struct sockaddr* address, unsigned short* seq
 
 int handshake(int sockfd, struct sockaddr* address, unsigned short* seqNum, unsigned short* ackNum){
     //creates handshake with server specified by address parameter
-
+    
     *seqNum = rand() % 25600; //assigning random sequence number
-
+    
     struct packet syn = {*seqNum, 0, 0, 1, 0, {0, 0, 0, 0, 0}, {}}, synack;
     socklen_t size = sizeof(struct sockaddr_in);
-
+    
     //send syn
     if (sendto(sockfd, (void*) &syn, 12, 0, address, size) == -1){
         fprintf(stderr, "Couldn't send SYN to server, %s\n", strerror(errno));
@@ -133,21 +153,21 @@ int handshake(int sockfd, struct sockaddr* address, unsigned short* seqNum, unsi
     }
     fprintf(stderr, "handshake\n");
     printsent(&syn, 0, 0);
-
+    
     //wait for ack
     if (recvfrom(sockfd, (void*) &synack, 12, 0, NULL, 0) == -1){
         fprintf(stderr, "Couldn't receive SYNACK from server, %s\n", strerror(errno));
         return -1;
     }
     if (synack.syn != 1)
-        return -1;
+    return -1;
     printreceived(&synack, 0, 0);
     *ackNum = synack.seqNum + 1;
     if (synack.ackNum != *seqNum + 1){
         fprintf(stderr, "synack returned incorrect ack number\n");
         return -1;
     }
-
+    
     (*seqNum)++;
     fprintf(stderr, "handshake complete\n\n\n");
     return 0;
@@ -169,10 +189,10 @@ long iptolong(char* ip){
             fprintf(stderr, "improper ip address\n");
             return -1;
         } else
-            a += ip[i] - '0';
+        a += ip[i] - '0';
         i++;
     }
-
+    
     result *= 256;
     result += a;
     return result;
@@ -186,7 +206,7 @@ int stringtoint(char* string){
     for (i = 0; i < size; i++){
         number *= 10;
         if (string[i] > '9' || string[i] < '0')
-            return -1;
+        return -1;
         number += string[i] - '0';
     }
     return number;
@@ -195,19 +215,19 @@ int stringtoint(char* string){
 
 
 int main(int argc, char* argv[]){
-
+    
     //handling command line arguments
     long servername;
     int port;
     char* filename;
-
+    
     if (argc != 4){
         fprintf(stderr, "Incorrect number of arguments.\nclient should be invoked with three arguments as\n./client <HOSTNAME-OR-IP> <PORT> <FILENAME>\n");
         exit(1);
     }
-
+    
     if (strcmp(argv[1], "localhost") == 0)
-        servername = 0x7f000001;
+    servername = 0x7f000001;
     else{
         servername = iptolong(argv[1]);
         if (servername == -1){
@@ -215,20 +235,20 @@ int main(int argc, char* argv[]){
             exit(1);
         }
     }
-
+    
     port = stringtoint(argv[2]);
     if (port < 0 || port > 65535){
         fprintf(stderr, "Improper port number\n");
         exit(1);
     }
-
+    
     filename = argv[3];
     int filefd = open(filename, O_RDONLY);
     if (filefd < 0){
         fprintf(stderr, "Couldn't open file %s, %s\n", filename, strerror(errno));
         exit(1);
     }
-
+    
     //setting up socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     unsigned short seqNum, ackNum;
@@ -242,9 +262,9 @@ int main(int argc, char* argv[]){
     struct timeval timeout = {10, 0};
     setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (void*) &timeout, sizeof(timeout));
     fcntl(sockfd, F_SETFD, O_NONBLOCK);
-
+    
     //create signal handler for SIGINT and SIGTERM, use globals
-
+    
     //handshake
     if (handshake(sockfd, addr, &seqNum, &ackNum) == -1){
         fprintf(stderr, "Handshake failed\n");
@@ -252,7 +272,7 @@ int main(int argc, char* argv[]){
         close(filefd);
         exit(1);
     }
-
+    
     //transmit file
     if (transmit(filefd, sockfd, addr, &seqNum, &ackNum) == -1){
         fprintf(stderr, "Transmission failed\n");
@@ -268,10 +288,10 @@ int main(int argc, char* argv[]){
         close(filefd);
         exit(1);
     }
-
+    
     //closing socket
     close(sockfd);
     close(filefd);
-
+    
     return 0;
 }
